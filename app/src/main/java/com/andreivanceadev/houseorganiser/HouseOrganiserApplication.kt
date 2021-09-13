676e674756cacf0c4add377a0771bf009c644aa5
Andrei Vancea <andrei.vancea.work@gmail.com>
2021-09-13 13:35:56 +0100

    Project setup

---
+++ app/src/main/java/com/andreivanceadev/houseorganiser/HouseOrganiserApplication.kt
@@ -0,0 +1,11 @@
+package com.andreivanceadev.houseorganiser
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HouseOrganiserApplication : Application() {
+
+
+
+}
